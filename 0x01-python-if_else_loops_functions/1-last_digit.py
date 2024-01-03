cfecda31583c74089c859bf97f727c41f70f4174
mahmoudhmoud <jadmclarence@gamil.com>
2024-01-03 14:59:11 -0800

    afafw

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 import math
-number  =  random.randint(-10000, 10000)
-mod  =  number % 10 if number > 10 else number % -10
+number = random.randint(-10000, 10000)
+mod = number % 10 if number > 10 else number % -10
 print(
     "Last digit of {:d} is {:d} and is "
     .format(number, mod), end="")
