7d73016b5e1f3ba5c72727e18d60c5246249db13
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-14 10:32:38 -0700

    afatrwe

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-''' kardahom fkaaima '''
+''' ifi ohkjf '''
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-''' kaysamah bach biaad '''
+    """ oh bey rtjl """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
