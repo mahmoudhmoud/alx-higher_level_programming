08795daa2c6a3dd1558991a976bd91dfaee2fa4c
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-14 16:27:24 -0700

    afdarteyrt

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+''' makdtob kyakad lkadj '''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+''' alfdla tofd for it '''
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
