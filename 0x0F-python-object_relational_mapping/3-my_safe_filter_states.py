c4b1556402faf98b2dfc9d3acee11d21b4e41605
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-14 16:31:56 -0700

    afdatswyers

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-''' makdtob kyakad lkadj '''
+''' yad ajal alij '''
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-
-''' alfdla tofd for it '''
+    ''' is to permit takahd '''
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
