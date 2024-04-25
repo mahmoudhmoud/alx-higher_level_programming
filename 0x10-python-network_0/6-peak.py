784690223dcddd17697d33e15241d1afb2774e21
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-25 10:50:32 -0700

    eyeyrzxv

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,7 +2,24 @@
 ''' logarithm dyal kaima '''
 
 def find_peak(list_of_integers):
-    ''' lka lkayim dyalo '''
-        if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    ''' lka lkayim dyalo man tam '''
+size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
