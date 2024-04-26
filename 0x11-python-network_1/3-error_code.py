4d8e95f596846c0e3919925ae681444c66423ffc
mahmoudhmoud <jadmclarence@gamil.com>
2024-04-26 09:47:48 -0700

    tertet

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' sifat talab rabit tam '''
+
+if __name__ == '__main__':
+    import sys
+    from urllib import request, error
+
+    argv = sys.argv
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.status))
