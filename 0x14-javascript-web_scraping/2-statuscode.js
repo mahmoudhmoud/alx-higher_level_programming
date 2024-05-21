e18d0a5181a5a9365ec872300b4af7622e1591c7
mahmoudhmoud <jadmclarence@gamil.com>
2024-05-21 15:58:40 -0700

    ryryr

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// tkhad lamar
+// mantam ldik likantya makwe
+
+const request = require("request");
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
+});
