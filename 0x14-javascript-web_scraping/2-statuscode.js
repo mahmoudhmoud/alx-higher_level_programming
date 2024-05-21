a25dcb71b27deb16cef3309c246c9c842696d9c7
mahmoudhmoud <jadmclarence@gamil.com>
2024-05-21 16:06:36 -0700

    rtyrty

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // tkhad lamar
 // mantam ldik likantya makwe
 
-const request = require("request");
+const request = require('request');
 const url = process.argv[2];
 request.get(url, function (error, response) {
   if (error) {
